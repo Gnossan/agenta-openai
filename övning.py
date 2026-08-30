3c7623795983bd53d06fcf7f348a2cd4e99dd456
Tomas Hultberg <tomas@gnossa.se>
2026-08-30 08:33:45 +0200

    Uppdatera config (v0.5.6), städa gamla testskript

--- övning.py
+++
@@ -1,8 +0,0 @@
-personer = [
-    {"namn": "Tomas", "ålder": 61},
-    {"namn": "Karl", "ålder": 45},
-    {"namn": "Ea", "ålder": 30}
-]
-
-for person in personer:
-    print(f"{person['namn']} är {person['ålder']} år")
